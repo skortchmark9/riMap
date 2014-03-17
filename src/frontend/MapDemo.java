fbbd589f693d5225f562681adcef077b2afa7b86
skortchmark9 <samuel_kortchmar@brown.edu>
2014-03-17 00:33:26 -0400

    Made the new AC class

--- src/frontend/MapDemo.java
+++ src/frontend/MapDemo.java
@@ -1,24 +1,24 @@
 package frontend;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
-import java.io.IOException;
 
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
-import backend.Backend;
-
-public class AutoCorrectDemo {
+public class MapDemo {
 	public static void main(String[] args) {
-		new AutoCorrectDemo();
+		new MapDemo();
 	}
 	
 
-    public AutoCorrectDemo() {
+    public MapDemo() {
 
         EventQueue.invokeLater(new Runnable() {
             @Override
@@ -35,19 +35,15 @@ public class AutoCorrectDemo {
                 JFrame frame = new JFrame();
                 frame = new JFrame();
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(800, 400);
+                frame.setSize(800, 800);
                 frame.setLocationRelativeTo(null);
                 frame.setLayout(new BorderLayout());
-                Backend b;
-				try {
-					String arg1 = "/Users/samkortchmar/Documents/Brown/Semester IV/CS032/Projects/CS032_Maps/data/mapsfiles/ways.tsv";
-					String arg2	= "/Users/samkortchmar/Documents/Brown/Semester IV/CS032/Projects/CS032_Maps/data/mapsfiles/nodes.tsv";
-					String arg3 = "/Users/samkortchmar/Documents/Brown/Semester IV/CS032/Projects/CS032_Maps/data/mapsfiles/index.tsv";
-					b = new Backend(new String[] {arg1, arg2, arg3});
-	                frame.add(new InputPane(b));
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+                final MapPane pane = new MapPane();
+                pane.setBorder(BorderFactory.createLineBorder(Color.blue));
+                JPanel p2 = new JPanel();
+                p2.setBackground(Color.WHITE);
+                frame.add(p2);
+                frame.add(new JScrollPane(pane));
                 frame.setVisible(true);
 
                 SwingUtilities.invokeLater(new Runnable() {
