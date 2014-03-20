d9645f81c3a625b482d1528d0cdfdd7e388c47da
eliath <eliasmartinezcohen@gmail.com>
2014-03-20 14:50:01 -0400

    fixed Suggestions to include #s

--- src/autocorrect/AutoCorrectConstants.java
+++ src/autocorrect/AutoCorrectConstants.java
@@ -2,7 +2,7 @@ package autocorrect;
 
 public class AutoCorrectConstants {
 	/** Holds constants for autocorrect. */
-	public final static String parsePattern = "\\s*[^a-z ]"; //used in lineparser for separating out words
+	public final static String parsePattern = "\\s*[^a-z0-9]"; //used in line parser for separating out words
 
 	public final static int maxRTD = 5; //Maximum nodes past the given word we will traverse in search of suffixes
 	
