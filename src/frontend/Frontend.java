df7968e36da6eddbc2f0f1c9e8c7b6a02fc069e5
skortchmark9 <samuel_kortchmar@brown.edu>
2014-03-20 18:36:18 -0400

    Implemented a timer on pathfinding operations

--- src/frontend/Frontend.java
+++ src/frontend/Frontend.java
@@ -1,7 +1,6 @@
 package frontend;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Cursor;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -11,7 +10,16 @@ import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import javax.swing.BoxLayout;
@@ -20,13 +28,11 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-import maps.MapFactory;
 import maps.Node;
 import maps.Way;
 import backend.Backend;
 import backend.Constants;
 import backend.Util;
-import backend.Backend.BackendType;
 
 public class Frontend implements ActionListener {
 	SearchAutoFillPane box1, box2, box3, box4;
@@ -38,6 +44,8 @@ public class Frontend implements ActionListener {
 	private AtomicInteger threadCount;
 	final Cursor defaultCursor = Cursor.getDefaultCursor();
 	final Cursor busyCursor = Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR);
+	List<Future<List<Way>>> futurePathWays = new LinkedList<>();
+	ExecutorService executor;
 	
 	Frontend(Backend b) {
 		this.b = b;
@@ -46,6 +54,7 @@ public class Frontend implements ActionListener {
 		frame.setExtendedState(Frame.MAXIMIZED_BOTH);
 		frame.setLayout(new FlowLayout());
 		frame.getContentPane().setBackground(Color.BLACK);
+		executor = Executors.newFixedThreadPool(5);
 		
 		
 		calcStreetNames = new JButton("Calculate from cross-streets");
@@ -121,11 +130,34 @@ public class Frontend implements ActionListener {
 			Node start = map.getStart();
 			Node end = map.getEnd();
 			if (start != null && end != null) {
-				new PathFindingThread(start, end).start();
+//				new PathFindingThread(start, end).start();
+				Future<List<Way>> ways = executor.submit(new CallableWays(start, end));
+				futurePathWays.add(ways);
 			}
+			List<Way> wayList = new LinkedList<>();
+			for(Future<List<Way>> ways : futurePathWays) {
+				try {
+					wayList = ways.get(5, TimeUnit.SECONDS);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (ExecutionException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				} catch (TimeoutException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			}
+			map.clearRoute();
+			if (Constants.DEBUG_MODE) {
+				Util.out("WAYS FOUND:", wayList);
+			}
+			map.setCalculatedRoute(wayList);
+			map.repaint();
 		}
 	}
-	
+		
 	private class PathFindingThread extends Thread {
 		private int numberID;
 		private Node start, end;
@@ -156,6 +188,24 @@ public class Frontend implements ActionListener {
 			}
 		}
 	}
+	
+	
+
+	class CallableWays implements Callable<List<Way>> {
+	
+		Node start, end;
+	
+		public CallableWays(Node start, Node end) {
+			this.start = start;
+			this.end = end;
+		}
+	
+		@Override
+		public List<Way> call() {
+			return b.getPath(start, end);
+		}
+	}
+
 
 	public static void main(String[] args) {
 		try {
