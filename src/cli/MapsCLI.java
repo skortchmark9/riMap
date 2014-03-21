38f7da59abb3e3401dbe3bccab144b9b95ff71ca
eliath <eliasmartinezcohen@gmail.com>
2014-03-20 23:17:17 -0400

    CLI comments

--- src/cli/MapsCLI.java
+++ src/cli/MapsCLI.java
@@ -7,25 +7,33 @@ import java.io.InputStreamReader;
 
 import backend.Backend;
 
+/**
+ * This class runs the command-line interface of the 
+ * program.
+ * @author skortchm
+ *
+ */
 public class MapsCLI implements Runnable{
 	
 	Backend b;
-
+	
+	/**
+	 * Default constructor.
+	 * sets its internal reference to the parametric backend and
+	 * runs itself.
+	 * @param b - the backend to use to drive the CLI
+	 */
 	public MapsCLI(Backend b) {
 		this.b = b;
 		run();
 	}
-	public static void main(String[] args) {
-		Backend b;
-		try {
-			b = new Backend(args);
-			new MapsCLI(b);
-		} catch (IOException e) {
-			System.err.print("ERROR: Could not load backend");
-			System.exit(1);
-		}
-	}
-	
+
+	/**
+	 * Runs the CLI.
+	 * spins forever to read from Standard in.
+	 * The app will exit when the user enters "exit", "quit", "EOF", or 
+	 * simple the empty string, which is usually sent with CTRL+D. 
+	 */
 	public void run() {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String userInput = "";
@@ -36,7 +44,7 @@ public class MapsCLI implements Runnable{
 			} catch (IOException ioe) {
 				ioe.printStackTrace();
 			}
-			if (userInput.equals("exit")) //Exit command
+			if (userInput.equals("exit") || userInput.equals("quit")) //Exit command
 				break;
 			else if (userInput.equals("EOF")) //Exit command
 				break;
