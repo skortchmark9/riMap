b2e8c5d45575191f47129229b214ec1af0fcf07e
eliath <eliasmartinezcohen@gmail.com>
2014-04-07 01:47:05 -0400

    renamed statResp

--- src/client/Client.java
+++ src/client/Client.java
@@ -174,8 +174,8 @@ public class Client {
 			//If the response is a server status, we'll print the message to the
 			//console or the loading screen, whichever is currently being used
 			//to display messages to the user.
-			ServerStatus sR = (ServerStatus) resp;
-			_frontend.guiMessage(sR.getMsg());
+			ServerStatus statResp = (ServerStatus) resp;
+			_frontend.guiMessage(statResp.getMsg());
 			break;
 		case WAYS:
 			//If the response is ways, we'll set the mapPane to display them
