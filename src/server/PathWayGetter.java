b5728282c9f178a628d51b17e5ebfb19b9ba4822
skortchmark9 <samuel_kortchmar@brown.edu>
2014-04-07 01:03:01 -0400

    Fixed PWgetter

--- src/server/PathWayGetter.java
+++ src/server/PathWayGetter.java
@@ -1,6 +1,5 @@
 package server;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -10,64 +9,71 @@ import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
+import backend.Util;
+import shared.PathResponse;
 import maps.Node;
 import maps.Way;
-import shared.PathResponse;
 
 /**
- * Class 
  * @author samkortchmar
- *Class for
+ * Should be changed probably
  */
-public class PathWayGetter {
+public class PathWayGetter implements Runnable {
 
-	ExecutorService _executor;
 	ClientHandler _owner;
+	CallableWays _worker;
+	volatile boolean _running;
+	ExecutorService _executor;
+	Future<List<Way>> waysFuture;
+	int _timeout;
 
 	public PathWayGetter(ClientHandler owner) {
 		_owner = owner;
-		_executor = Executors.newFixedThreadPool(5);
+		_running = true;
+		_executor = Executors.newSingleThreadExecutor();
+		run();
 	}
 
 	public void findPath(Node start, Node end, int seconds) {
-		Runnable worker = new FinderThread(start, end, seconds);
-		_executor.execute(worker);
+		if (waysFuture != null) {
+			waysFuture.cancel(true);
+		}
+		_timeout = seconds;
+		waysFuture = _executor.submit(new CallableWays(start, end));
 	}
 
-	private class FinderThread implements Runnable {
+	public void close() {
+		_running = false;
+	}
 
-		private int _timeout;
-		private Node _start, _end;
-
-		FinderThread(Node start, Node end, int timeout) {
-			_timeout = timeout;
-			_start = start;
-			_end = end;
-		}
-
-		@Override
-		public void run() {
-			Future<List<Way>> future = _executor.submit(new CallableWays(_start, _end));
-			List<Way> wayList = new LinkedList<>();
-			PathResponse pr = null;
-			try {
-				wayList = future.get(_timeout, TimeUnit.SECONDS);
-				if (wayList.isEmpty()) {
-					pr = new PathResponse("Sorry, could not find a path");
+	@Override
+	public void run() {
+		while(_running) {
+			if (waysFuture != null) {
+				try {
+					List<Way> ways = waysFuture.get(_timeout, TimeUnit.SECONDS);
+					if (ways.isEmpty()) {
+						_owner._responseQueue.add(new PathResponse("Could not find a path."));
+					} else {
+						_owner._responseQueue.add(new PathResponse(ways));
+					}
+				} catch (InterruptedException e) {
+					continue;
+				} catch (ExecutionException e) {
+					//FIXME
+					Util.err("ERROR: Execution Exception in PathWayGetter...not sure why");
+					e.printStackTrace();
+				} catch (TimeoutException e) {
+					_owner._responseQueue.add(new PathResponse("Timed out after: " + _timeout + " seconds"));
 				}
-			} catch (InterruptedException e1) {
-				return;
-			} catch (ExecutionException e1) {
-			} catch (TimeoutException e1) {
-				pr = new PathResponse("The search timed out");
+				 waysFuture = null;
 			}
-			if (pr != null)
-				_owner._responseQueue.add(pr);
 		}
 	}
+
+
 
 	class CallableWays implements Callable<List<Way>> {
-
 		Node start, end;
 
 		public CallableWays(Node start, Node end) {
