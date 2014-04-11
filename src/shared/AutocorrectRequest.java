3423ac9b628da7228d5cbe50ca9a2a6d4991be8b
skortchmark9 <samuel_kortchmar@brown.edu>
2014-04-10 22:56:16 -0400

    Erroneous p

--- src/shared/AutocorrectRequest.java
+++ src/shared/AutocorrectRequest.java
@@ -3,6 +3,7 @@ package shared;
 /**
  * A Request for autocorrections - given the specific input
  * @author samkortchmar
+ * Note that more detailed comments are available in the Request Interface
  */
 public class AutocorrectRequest implements Request {
 
